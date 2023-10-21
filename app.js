0f6215ec144638279ded58ffe8541bc5e8dc8bbe
sujonsarkercmt <sujonsarkercmtspi@gmail.com>
2023-10-21 20:40:28 +0600

    sss

--- app.js
+++ app.js
@@ -75,7 +75,14 @@ profile.connectToDatabase();
 const login = require('./database/login');
 login.connectToDatabase();
 
+app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 
 //========================================================================================

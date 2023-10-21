9d0a81879c4fbe8ecccbbdee8a75296f24f43393
sujonsarkercmt <sujonsarkercmtspi@gmail.com>
2023-10-21 20:42:42 +0600

    sasd

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.use('/login', loginr);
 //========================================================================================
 //                                Server Start
 //========================================================================================
-server.listen(3000, () => {
-    console.log(`Server is running on port`, 3000);
+server.listen(80, () => {
+    console.log(`Server is running on port`, 80);
 });
 
